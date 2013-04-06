d9731f7f021c9ff034c5b614d51bd91899c35971
John Michel <cowbird@gmail.com>
2013-04-05 21:16:32 -0400

    Add WAT.js

---
+++ WAT.js
@@ -0,0 +1,34 @@
+// WAT.js 0.0.1
+
+// (c) 2013
+
+// Initial setup. Using Underscore as a template. Thanks, @ashkenas.
+// -----------
+(function() {
+    var root = this;
+
+    var previousWAT = root.WAT;
+
+    var WAT = function(obj) {
+        if (obj instanceof WAT) return obj;
+        if (!(this instanceof WAT)) return new WAT(obj);
+        this.WATwrapped = obj;
+    };
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = WAT;
+        }
+        exports.WAT = WAT;
+    }
+    else {
+        root.WAT = WAT;
+    }
+
+    WAT.VERSION = '0.0.1';
+
+    WAT.greet = function() {
+        return 'WAT';
+    };
+
+}).call(this);
